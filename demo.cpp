d78e68450288858e1d369dd6a8dce5a4c0aca5e8
ted <linyc1983@gmail>
2017-03-10 11:41:26 +0800

    first commit

---
+++ demo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+// 引入標準程式庫中相關的字串程式
+#include <string>
+ 
+// std 為標準程式庫的名稱空間
+using namespace std;
+ 
+int main(void)
+{
+    // 此行程式碼建立名稱為 m ， string 型態的物件
+    // 小括弧為 string 的建構子
+    // 雙引號為字串字面常數
+    string m("There is no spoon.");
+     
+    // cout 為用來輸出的物件
+    // endl 為新行符號 \n
+    cout << endl;
+    cout << m << endl;    
+    cout << endl << endl;
+     
+    return 0;
+}
+ 
+/* 《程式語言教學誌》的範例程式
+    http://pydoing.blogspot.com/
+    檔名：demo.cpp
+    功能：示範 C++ 程式 
+    作者：張凱慶
+    時間：西元 2012 年 10 月 */
